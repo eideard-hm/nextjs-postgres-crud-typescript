fdda090c9463eab132d1ece0a121e9eb40378c8a
eideard-hm <ehhernandez81@misena.edu.co>
2022-03-04 10:51:10 -0500

    Finish the rest api with nextjs and postgress

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,16 +1,41 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
+import { conn } from "../../../utils/postgres";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    const { method } = req;
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { method, body, query: { id } } = req;
 
     switch (method) {
         case 'GET':
-            return res.json('Getting a unique task');
+            try {
+                const taskById = await conn.query('SELECT * FROM tasks WHERE id = $1',
+                    [id]);
+                if (taskById.rows.length === 0)
+                    return res.status(400).json({ message: 'task not found' });
+                return res.status(200).json(taskById.rows);
+            } catch (error: any) {
+                return res.status(500).json({ error: error.message });
+            }
         case 'PUT':
-            return res.json('Modify a unique task');
+            const { title, description } = body;
+            try {
+                const updatedTask = await conn.query('UPDATE tasks SET title = $1, description = $2 WHERE id = $3', [title, description, id]);
+
+                if (updatedTask.rowCount === 0)
+                    return res.status(400).json({ message: 'Task not found' });
+                return res.status(204).send('');
+            } catch (error: any) {
+                return res.status(500).json({ error: error.message });
+            }
         case 'DELETE':
-            return res.json('Delete a task');
+            try {
+                const response = await conn.query('DELETE FROM tasks WHERE id = $1', [id]);
+                if (response.rowCount === 0)
+                    return res.status(400).json({ message: 'Task not found' });
+                return res.status(200).send('');
+            } catch (error: any) {
+                return res.status(500).json({ error: error.message });
+            }
         default:
             return res.status(400).json('Method not allowed');
     }
--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -7,15 +7,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     switch (method) {
         case 'GET':
-            return res.json('Getting data');
+            try {
+                const tasks = await conn.query('SELECT * FROM tasks');
+                return res.status(200).json(tasks.rows);
+            } catch (error: any) {
+                return res.status(500).json({error: error.message});
+            }
         case 'POST':
             const { title, description } = body;
-            const response = await conn.query('INSERT INTO tasks(title, description) VALUES($1, $2) RETURNING *', [title, description]);
-            return res.status(201).json(response.rows[0]);
-        case 'PUT':
-            return res.json('Modify data');
-        case 'DELETE':
-            return res.json('Delete data');
+            try {
+                const task = await conn.query('INSERT INTO tasks(title, description) VALUES($1, $2) RETURNING *', [title, description]);
+                return res.status(201).json(task.rows[0]);
+            } catch (error: any) {
+                return res.status(500).json({error: error.message});
+            }
         default:
             return res.status(400).json('Invalid Method')
     }
